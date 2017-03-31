0f57885797407aac6cb8943ad2af1ab46c621f12
pavic989 <pavicmilos989@gmail.com>
2017-03-31 16:13:38 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+function sleep(){
+		i++;
+		if( i > 2 ){
+			i = 0;
+			ar = makeText(); // reset
+		}
+		scroll.innerHTML = '';
+		start();
+}
 
+function makeText(){
+	var text = "Frontend Developer";
+	var text1 = "Hiring?";
+	var text2 = "Contact me!";
+
+	var stext = text.split('');
+	var stext1 = text1.split('');
+	var stext2 = text2.split('');
+	var nasArray = [stext, stext1, stext2];
+	return nasArray;
+}
+
+
+var ar = makeText();
+var scroll = document.getElementById('scroll');
+var loop;
+var i = 0;
+
+
+function start(){
+	if(ar[i].length > 0){
+		scroll.innerHTML += ar[i].shift();
+		loop = setTimeout(start,200);  // speed settings
+	} else {
+		var ss = setTimeout(sleep , 2000);
+	}
+}
+
+start();
